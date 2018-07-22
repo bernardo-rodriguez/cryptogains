d7d701c4bd2fc0a4df856622fa270e71c4b1ad65
Garrett Wesley <admin@airbears2-10-142-92-105.airbears2.1918.berkeley.edu>
2018-07-21 19:02:11 -0700

    Auth success

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,131 @@
-import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'dart:async';
+import 'dart:io';
 
-void main() => runApp(new MyApp());
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+final FirebaseAuth _auth = FirebaseAuth.instance;
+final GoogleSignIn _googleSignIn = new GoogleSignIn();
+
+void main() {
+  runApp(new MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp();
-
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Test',
-      home: const MyHomePage(title: 'Test'),
+      home: new MyHomePage(title: 'Login'),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key key, this.title}) : super(key: key);
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
 
   final String title;
 
   @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Future<String> _message = new Future<String>.value('');
+  TextEditingController _emailController = new TextEditingController();
+  TextEditingController _passwordController = new TextEditingController();
+  String verificationId;
+
+  Future<String> _testSignInWithGoogle() async {
+    final GoogleSignInAccount googleUser = await _googleSignIn.signIn();
+    final GoogleSignInAuthentication googleAuth =
+        await googleUser.authentication;
+    final FirebaseUser user = await _auth.signInWithGoogle(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+    assert(user.email != null);
+    assert(user.displayName != null);
+    assert(!user.isAnonymous);
+    assert(await user.getIdToken() != null);
+
+    final FirebaseUser currentUser = await _auth.currentUser();
+    assert(user.uid == currentUser.uid);
+
+    return 'signInWithGoogle succeeded: $user';
+  }
+  Future<String> _testSignIn() async {
+    final FirebaseUser user = await _auth.signInWithEmailAndPassword(
+      email: _emailController.text,
+      password: _passwordController.text,
+    );
+    print(user);
+    assert(user.email != null);
+    assert(user.displayName != null);
+    assert(!user.isAnonymous);
+    assert(await user.getIdToken() != null);
+    final FirebaseUser currentUser = await _auth.currentUser();
+    assert(user.uid == currentUser.uid);
+
+    return 'signIn succeeded: $user';
+  }
+
+  @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(title: new Text(title)),
-      body: new StreamBuilder(
-          stream: Firestore.instance.collection('data').snapshots(),
-          builder: (context, snapshot) {
-            if (snapshot.hasData) print(snapshot.data.documents);
-            return new ListView.builder(
-              itemCount: snapshot.data.documents.length,
-              padding: const EdgeInsets.only(top: 10.0),
-              itemExtent: 25.0,
-              itemBuilder: (context, index) {
-                DocumentSnapshot ds = snapshot.data.documents[index];
-                return new Text(" ${ds['curr_balance']}");
-              }
-            );
-          }),
+      appBar: new AppBar(
+        title: new Text(widget.title),
+      ),
+      body: new Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          new Container (
+            margin: const EdgeInsets.only(
+              top: 8.0,
+              bottom: 8.0,
+              left: 16.0,
+              right: 16.0,
+            ),
+            child: new Column ( 
+                children: [
+                    new TextField (
+                        controller: _emailController,
+                        autocorrect: false,
+                        decoration: const InputDecoration( hintText: 'Email' )
+                    ),
+                    new TextField (
+                        controller: _passwordController,
+                        autocorrect: false,
+                        obscureText: true,
+                        decoration: const InputDecoration( hintText: 'Password' )
+                    ),
+                    new RaisedButton(
+                        child: const Text('Sign in'),
+                        onPressed: () {
+                            setState(() {
+                            _message = _testSignIn();
+                            });
+                        }
+                    ),
+                ])
+          ),
+          new RaisedButton(
+                child: const Text('Sign in with Googgle'),
+                onPressed: () {
+                    setState(() {
+                    _message = _testSignInWithGoogle();
+                    });
+                }
+            ),
+          new FutureBuilder<String>(
+              future: _message,
+              builder: (_, AsyncSnapshot<String> snapshot) {
+                return new Text(snapshot.data ?? '',
+                    style: const TextStyle(
+                        color: const Color.fromARGB(255, 0, 155, 0)));
+              }),
+        ],
+      ),
     );
   }
 }
