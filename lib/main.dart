fae8266d49a54e5e9fddf21f01992d9b1b91d390
= <bernardoarodriguez@gmail.com>
2018-07-22 15:58:54 -0700

    initial swipable tabls with ugly crypto list

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,10 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
+import 'tabs/first.dart';
+import 'tabs/second.dart';
+import 'tabs/third.dart';
+
 final FirebaseAuth _auth = FirebaseAuth.instance;
 final GoogleSignIn _googleSignIn = new GoogleSignIn();
 
@@ -16,7 +20,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      home: new MyHomePage(title: 'Login'),
+      //home: new MyHomePage(title: 'Login'),
+      home: new MyHome(),
     );
   }
 }
@@ -54,6 +59,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
     return 'signInWithGoogle succeeded: $user';
   }
+
   Future<String> _testSignIn() async {
     final FirebaseUser user = await _auth.signInWithEmailAndPassword(
       email: _emailController.text,
@@ -79,44 +85,38 @@ class _MyHomePageState extends State<MyHomePage> {
       body: new Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          new Container (
-            margin: const EdgeInsets.only(
-              top: 8.0,
-              bottom: 8.0,
-              left: 16.0,
-              right: 16.0,
-            ),
-            child: new Column ( 
-                children: [
-                    new TextField (
-                        controller: _emailController,
-                        autocorrect: false,
-                        decoration: const InputDecoration( hintText: 'Email' )
-                    ),
-                    new TextField (
-                        controller: _passwordController,
-                        autocorrect: false,
-                        obscureText: true,
-                        decoration: const InputDecoration( hintText: 'Password' )
-                    ),
-                    new RaisedButton(
-                        child: const Text('Sign in'),
-                        onPressed: () {
-                            setState(() {
-                            _message = _testSignIn();
-                            });
-                        }
-                    ),
-                ])
-          ),
+          new Container(
+              margin: const EdgeInsets.only(
+                top: 8.0,
+                bottom: 8.0,
+                left: 16.0,
+                right: 16.0,
+              ),
+              child: new Column(children: [
+                new TextField(
+                    controller: _emailController,
+                    autocorrect: false,
+                    decoration: const InputDecoration(hintText: 'Email')),
+                new TextField(
+                    controller: _passwordController,
+                    autocorrect: false,
+                    obscureText: true,
+                    decoration: const InputDecoration(hintText: 'Password')),
+                new RaisedButton(
+                    child: const Text('Sign in'),
+                    onPressed: () {
+                      setState(() {
+                        _message = _testSignIn();
+                      });
+                    }),
+              ])),
           new RaisedButton(
-                child: const Text('Sign in with Googgle'),
-                onPressed: () {
-                    setState(() {
-                    _message = _testSignInWithGoogle();
-                    });
-                }
-            ),
+              child: const Text('Sign in with Googgle'),
+              onPressed: () {
+                setState(() {
+                  _message = _testSignInWithGoogle();
+                });
+              }),
           new FutureBuilder<String>(
               future: _message,
               builder: (_, AsyncSnapshot<String> snapshot) {
@@ -125,6 +125,74 @@ class _MyHomePageState extends State<MyHomePage> {
                         color: const Color.fromARGB(255, 0, 155, 0)));
               }),
         ],
+      ),
+    );
+  }
+}
+
+class MyHome extends StatefulWidget {
+  @override
+  MyHomeState createState() => new MyHomeState();
+}
+
+// SingleTickerProviderStateMixin is used for animation
+class MyHomeState extends State<MyHome> with SingleTickerProviderStateMixin {
+  // Create a tab controller
+  TabController controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Initialize the Tab Controller
+    controller = new TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    // Dispose of the Tab Controller
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      // Appbar
+      appBar: new AppBar(
+        // Title
+        title: new Text("Using Bottom Navigation Bar"),
+        // Set the background color of the App Bar
+        backgroundColor: Colors.blue,
+      ),
+      // Set the TabBar view as the body of the Scaffold
+      body: new TabBarView(
+        // Add tabs as widgets
+        children: <Widget>[new FirstTab(), new EntryWidget(), new ThirdTab()],
+        // set the controller
+        controller: controller,
+      ),
+      // Set the bottom navigation bar
+      bottomNavigationBar: new Material(
+        // set the color of the bottom navigation bar
+        color: Colors.blue,
+        // set the tab bar as the child of bottom navigation bar
+        child: new TabBar(
+          tabs: <Tab>[
+            new Tab(
+              // set icon to the tab
+              icon: new Icon(Icons.favorite),
+            ),
+            new Tab(
+              icon: new Icon(Icons.adb),
+            ),
+            new Tab(
+              icon: new Icon(Icons.airport_shuttle),
+            ),
+          ],
+          // setup the controller
+          controller: controller,
+        ),
       ),
     );
   }
